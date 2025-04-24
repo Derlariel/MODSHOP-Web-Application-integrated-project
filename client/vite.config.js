ac9f3ca1ea535883c6caf0a578c2374a1c60b279
KONGPHOB KONGSAN <kongphob.kong@kmutt.ac.th>
2025-04-24 14:19:54 +0700

    convert client from submodule to folder

---
+++ client/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
+export default defineConfig({
+  resolve:{
+    alias: {
+      '@': '/src',
+    }
+  },
+  plugins: [
+    vue(),
+    tailwindcss(),
+    {
+      daisyui: {
+        themes: ["light", "dracula"],
+      },
+    },
+  ],
+});
