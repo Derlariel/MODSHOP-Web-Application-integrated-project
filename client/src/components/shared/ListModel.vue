c9e82544a75bebec2766b8a1d630a815ba39294e
KONGPHOB KONGSAN <142288866+Derlariel@users.noreply.github.com>
2025-05-01 10:31:24 +0700

    004- update ListModel.vue

--- client/src/components/shared/ListModel.vue
+++ client/src/components/shared/ListModel.vue
@@ -1,81 +1,30 @@
 <script setup>
-import { List, GalleryThumbnails } from "lucide-vue-next";
-import { ref } from "vue";
-import SideBar from "../shared/SideBar.vue";
-
-const isListView = ref(false);
-
 const props = defineProps({
-  products: Object,
-  productImages: Object,
+  saleItems: {
+    type: Array,
+    required: true,
+  },
+  viewType: {
+    type: String,
+    default: "gallery",
+    validator(value) {
+      return ["gallery", "list"].includes(value);
+    },
+  },
 });
 </script>
 
 <template>
-  <div class="flex">
-    
-    <!-- Sidebar -->
-    <SideBar />
-
-    <!-- Main Content -->
-    <div class="flex-1 p-6">
-  <p class="text-center font-extrabold text-2xl">Product List</p>
-
-      <!-- View Toggle -->
-      <div class="text-right mb-6">
-        <button
-          @click="isListView = !isListView"
-          class="text-sm px-4 py-2 rounded-full bg-gray-200 hover:bg-gray-300 transition-transform duration-300 hover:scale-110"
-        >
-          <component
-            :is="isListView ? List : GalleryThumbnails"
-            class="w-5 h-5"
-          />
-        </button>
+  <div class="container mx-auto px-4">
+    <div v-if="viewType === 'gallery'" class="itmbs-row grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+      <div v-for="(item, index) in saleItems" :key="index">
+        <slot name="listItems" :Item="item" :viewType="viewType"></slot>
       </div>
-
-      <!-- Product Gallery View -->
-      <ul
-        v-if="products.length && !isListView"
-        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6"
-      >
-        <li
-          v-for="product in products"
-          :key="product.id"
-          class="bg-white shadow-lg rounded-2xl overflow-hidden hover:shadow-xl transition-shadow"
-        >
-          <!-- Image -->
-          <img
-            :src="productImages[product.id]"
-            class="w-full h-60 object-contain bg-gray-100"
-          />
-          <!-- Details -->
-          <div class="p-4">
-            <h2 class="text-lg font-semibold text-gray-800">
-              {{ product.brandName }}
-            </h2>
-            <p class="text-gray-700 font-medium mb-1">
-              Model: {{ product.model }}
-            </p>
-            <p class="text-sm text-gray-600 mb-2">
-              RAM: {{ product.ramGb }} GB / {{ product.storageGb }} GB
-            </p>
-            <p class="text-green-600 font-bold text-xl">
-              ฿{{ product.price }}
-            </p>
-            <div class="flex justify-end pt-2">
-              <button class="bg-blue-500 px-3 py-1 rounded-lg text-white hover:bg-blue-700">
-                View
-              </button>
-            </div>
-          </div>
-        </li>
-      </ul>      
-      
-      <!-- Product List View -->
-      
-      <!-- No Products -->
-      <p v-else class="itmbs-message text-center  text-gray-400 py-10">No items found</p>
+    </div>
+    <div v-if="viewType === 'list'" class="itmbs-row space-y-4">
+      <div v-for="(item, index) in saleItems" :key="index" class="border-b-1 border-gray-300 p-4">
+        <slot name="listItems" :Item="item" :viewType="viewType"></slot>
+      </div>
     </div>
   </div>
 </template>
