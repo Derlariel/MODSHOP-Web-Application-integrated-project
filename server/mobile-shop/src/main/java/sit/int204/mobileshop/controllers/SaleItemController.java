ee3a347af990b8f45f5d9951dcf05fa9dcc3b6f3
napatzzz <napat.chum@mail.kmutt.ac.th>
2025-05-03 00:15:59 +0700

    027 update url for backend

--- server/mobile-shop/src/main/java/sit/int204/mobileshop/controllers/SaleItemController.java
+++ server/mobile-shop/src/main/java/sit/int204/mobileshop/controllers/SaleItemController.java
@@ -11,7 +11,7 @@ import sit.int204.mobileshop.utils.ListMapper;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4173")
+@CrossOrigin(origins = "http://ip24kk1.sit.kmutt.ac.th")
 @RestController
 @RequestMapping("/v1/sale-items")
 public class SaleItemController {
