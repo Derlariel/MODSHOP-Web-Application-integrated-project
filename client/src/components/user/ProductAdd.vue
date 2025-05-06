dc06b5601f4fcfffbf293b1a6f8912d738654a29
napatzzz <napat.chum@mail.kmutt.ac.th>
2025-05-06 13:06:51 +0700

    027

---
+++ client/src/components/user/ProductAdd.vue
@@ -0,0 +1,54 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <div>
+        
+    </div>
+    <div>
+        <div>
+            <p class="itbms-brand">Brand</p>
+            <select name="" id="">
+                <option value=""></option>
+            </select>
+        </div>
+        <div>
+            <p class="itbms-model">Model</p>
+            <input type="text">
+        </div>
+        <div>
+            <p>Price (Baht)</p>
+            <input type="text">
+        </div>
+        <div>
+            <p class="description">Description</p>
+            <textarea name="" id=""></textarea>
+        </div>
+        <div>
+            <p class="ramGb">Ram (GB)</p>
+            <input type="text">
+        </div>
+        <div>
+            <p class="screenSizeInch">screen Size (Inches)</p>
+            <input type="text">
+        </div>
+        <div>
+            <p class="itbms-storageGb">Storage (GB)</p>
+            <input type="text">
+        </div>
+        <div>
+            <p class="itbms-color">Color</p>
+            <input type="text">
+        </div>
+        <div>
+            <p class="itbms-quantity">Quantity</p>
+            <input type="text">
+        </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
