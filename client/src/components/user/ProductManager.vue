9d93802ce65f618f69169fa266a21d58e0477797
napatzzz <napat.chum@mail.kmutt.ac.th>
2025-05-06 11:29:18 +0700

    027 update no cache on frontend server

--- client/src/components/user/ProductManager.vue
+++ client/src/components/user/ProductManager.vue
@@ -6,22 +6,15 @@ import { useRoute } from "vue-router";
 const route = useRoute()
 
 const productStore = useProductStore();
-const message = ref(null)
 
 onMounted(async () => {
     await productStore.loadProducts();
-
-    message.value = sessionStorage.getItem('error-message');
-    sessionStorage.removeItem('error-message');
     
 });
 </script>
  
 <template>
 <div>
-    <div v-if="message">
-        <h1 class="itbms-message">{{ message }}</h1>
-    </div>
     <ProductList v-if="!['product-detail'].includes(route.name) && !message"/>
     <router-view></router-view>
 </div>
