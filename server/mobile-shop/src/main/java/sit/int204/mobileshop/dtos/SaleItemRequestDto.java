b6a3a8fbc33af442fde2574cd5bc912b0799782f
Derlariel <kongphob.kong@kmutt.ac.th>
2025-05-13 00:59:01 +0700

    004 test postman script

--- server/mobile-shop/src/main/java/sit/int204/mobileshop/dtos/SaleItemRequestDto.java
+++ server/mobile-shop/src/main/java/sit/int204/mobileshop/dtos/SaleItemRequestDto.java
@@ -3,6 +3,7 @@ package sit.int204.mobileshop.dtos;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,10 @@ import java.math.BigDecimal;
 import java.time.Instant;
 @Getter @Setter
 public class SaleItemRequestDto {
+
+    @NotBlank
+    @Size(min = 12)
+    private String name;
 
     @NotBlank
     private String model;
