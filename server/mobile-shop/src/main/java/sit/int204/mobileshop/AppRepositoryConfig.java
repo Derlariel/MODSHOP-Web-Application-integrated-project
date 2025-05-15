7bcb0afc9bbfa83be10a01465a5c7c60a0aa3f6c
Derlariel <kongphob.kong@kmutt.ac.th>
2025-05-15 23:12:40 +0700

    004 test reverse proxy v18

--- server/mobile-shop/src/main/java/sit/int204/mobileshop/AppRepositoryConfig.java
+++ server/mobile-shop/src/main/java/sit/int204/mobileshop/AppRepositoryConfig.java
@@ -1,6 +1,5 @@
 package sit.int204.mobileshop;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -9,17 +8,9 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 @Configuration
 public class AppRepositoryConfig implements RepositoryRestConfigurer {
 
-    @Value("${app.origins}")
-    private String origins;
-
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 
         config.setExposeRepositoryMethodsByDefault(false);
-
-        cors.addMapping("/**")
-                .allowedOrigins(origins)
-                .allowedMethods("GET","POST","PUT","DELETE" , "OPTIONS")
-                .allowCredentials(true);
     }
 }
