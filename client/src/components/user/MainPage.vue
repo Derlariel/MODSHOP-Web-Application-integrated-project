3f03159beaff2f81fbc8dc22fd21f3554c64bd34
Derlariel <kongphob.kong@kmutt.ac.th>
2025-05-15 20:06:44 +0700

    004 build new dist

--- client/src/components/user/MainPage.vue
+++ client/src/components/user/MainPage.vue
@@ -35,6 +35,9 @@ const productStore = useProductStore()
           />
         </SwiperSlide>
       </Swiper>
+      <div class="text-center">
+        สวัสดีจร้า
+      </div>
     </div>
   </template>
   
