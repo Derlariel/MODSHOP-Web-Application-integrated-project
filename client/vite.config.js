96cb8ac5c8002da4e9443a2305136df9feb84149
napatzzz <napat.chum@mail.kmutt.ac.th>
2025-05-16 02:48:53 +0700

    027 upadate allow server intproj

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  base: '/kk1/',
+  base: '/kk1/itb-mshop/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)), 
