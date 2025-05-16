25c19bad5dab7f24f88d4b22f1245596f7dd7e74
napatzzz <napat.chum@mail.kmutt.ac.th>
2025-05-17 00:07:33 +0700

    027 update test for follow post man

--- server/mobile-shop/src/main/java/sit/int204/mobileshop/dtos/BrandInfoDto.java
+++ server/mobile-shop/src/main/java/sit/int204/mobileshop/dtos/BrandInfoDto.java
@@ -10,7 +10,7 @@ public class BrandInfoDto {
     private Integer id;
 
     @NotBlank
-    @Size(max = 30, message = "Brand name must not exceed 30 characterss")
+    @Size(max = 30, message = "Brand name must not exceed 30 characters")
     private String name;
 
     @Size(max = 40, message = "Website URL must not exceed 40 characters")
