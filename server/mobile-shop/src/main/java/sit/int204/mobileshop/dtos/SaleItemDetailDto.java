541134995a78f53135a00c8be95f7931c2fa5444
napatzzz <napat.chum@mail.kmutt.ac.th>
2025-05-29 13:39:19 +0700

    027 update pbi 13

--- server/mobile-shop/src/main/java/sit/int204/mobileshop/dtos/SaleItemDetailDto.java
+++ server/mobile-shop/src/main/java/sit/int204/mobileshop/dtos/SaleItemDetailDto.java
@@ -24,8 +24,7 @@ public class SaleItemDetailDto {
     @Min(value = 0)
     private Integer storageGb;
     private String color;
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssXXX", timezone = "Asia/Bangkok")
     private Instant createdOn;
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssXXX", timezone = "Asia/Bangkok")
+  
     private Instant updatedOn;
 }
