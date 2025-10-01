84bf7c2d2ff1bd524b583ae615952679fae6a703
Derlariel <kmnj05as@gmail.com>
2025-10-01 21:12:13 +0700

    test

--- server/mobile-shop/src/main/java/sit/int204/mobileshop/controllers/UserController.java
+++ server/mobile-shop/src/main/java/sit/int204/mobileshop/controllers/UserController.java
@@ -9,13 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
